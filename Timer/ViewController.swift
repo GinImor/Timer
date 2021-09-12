ccf9d9d7ebcc6e17ac85a2ccc3b9b045cf0c7ab7
Gin Imor <openheart23@163.com>
2021-09-12 09:46:55 +0800

    Initial set up

---
+++ Timer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Timer
+//
+//  Created by Gin Imor on 9/12/21.
+//  
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
